6e1e364048ca8ef2cd3eed87df3e7c7e8d8ce03f
magu1216 <61665279+magu1216@users.noreply.github.com>
2021-01-20 11:51:55 +0100

    nothing important

--- src/components/AboutMeSection.js
+++ src/components/AboutMeSection.js
@@ -4,10 +4,25 @@ import styled from "styled-components";
 export default function AboutMeSection() {
 	return (
 		<AboutMeWrapper id='aboutme'>
+			<TextBox>
+				<h2>Location</h2>
+				<p>Munich</p>
+			</TextBox>
 			<img
 				src='https://profile-images.xing.com/images/c84a69278abeff4d24088560acefdbb3-4/markus-g%C3%BCrtner.1024x1024.jpg'
 				alt='picture_of_myself'
 			></img>
+			<TextBox>
+				<h1>Motivation</h1>
+				<p>
+					Aus dem Maschinenbau kommend freue ich mich nun
+					auf meinen neuen Weg in der IT. Mit einem modernen
+					Mindset und aktuellsten Tools die Welt von morgen
+					mitzugestalten ist mein persönlicher Antrieb. Ich
+					bin gespannt, welche Herausforderungen von mir
+					gemeistert werden wollen.
+				</p>
+			</TextBox>
 		</AboutMeWrapper>
 	);
 }
@@ -28,4 +43,9 @@ const AboutMeWrapper = styled.section`
 		width: 400px;
 		border-radius: 200px;
 	}
+`;
+
+const TextBox = styled.div`
+	width: 20vw;
+	margin: 20px;
 `;
